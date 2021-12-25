3ed5d3b5a256d8d348d112e0674cc4315495d3c5
hunmin815 <hunmin815@naver.com>
2021-12-25 17:36:13 +0900

    Day 2-1 : Socket Open & Close & Send Message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,23 @@
 // Front End Zone
-alert("hi");
+
+const socket = new WebSocket(`ws://${window.location.host}`); // localhost websocket 열기
+
+// Socket 연결 시
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✔");
+});
+
+// message 받기 (Back -> Front)
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data, " from the server");
+});
+
+// Socket 종료 시
+socket.addEventListener("close", () => {
+  console.log("DisConnected from Server X");
+});
+
+// Socket 메세지 전달 (Front -> Back)
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express"; // express 사용 선언
+import http from "http";
+import WebSocket from "ws";
 
 const app = express(); // express 함수 호출
 
@@ -7,7 +9,33 @@ app.set("views", __dirname + "/views"); // 뷰 경로 설정, __dirname = 현재
 app.use("/public", express.static(__dirname + "/public")); // app.use = 모든 요청을 받음
 
 app.get("/", (req, res) => res.render("home")); // only get 요청만 받음
+app.get("/*", (req, res) => res.redirect("/")); // 이외 URL 요청은 /로 redirect
 
 console.log("Hi");
 
-app.listen(3333); // listen Port
+// app.listen(3333); // listen Port
+
+const server = http.createServer(app); // http 서버 생성
+
+const wss = new WebSocket.Server({ server }); // websocket과 http 둘다 사용하는 방법 (하나의 포트로 모두 처리)
+
+// Socket 종료 시
+function onSocketClose() {
+  console.log("DisConnected from Browser X");
+}
+
+// Socket 메세지 받기 (Front -> Back)
+function onSocketMessage_rec(message) {
+  console.log(message.toString("utf-8"));
+}
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✔");
+  socket.on("close", onSocketClose);
+  // Browser Message 받기
+  socket.on("message", onSocketMessage_rec);
+  //
+  socket.send("hello~!"); // Socket 메세지 보내기 (Back -> Front)
+});
+
+server.listen(3333);
